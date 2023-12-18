b165c61824ba76b61d8f969f49eb6a25fdf53491
nabilchabab <nabil.chababnabil@gmail.com>
2023-12-18 11:02:25 +0100

    First Commit

---
+++ app/Database/Database.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace MyApp\Database;
+
+require '../../vendor/autoload.php';
+
+class Database {
+    private static $cnx;
+
+    public static function connect() {
+        $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
+        $dotenv->load();
+
+        $dsn = "mysql:host={$_ENV['DB_SERVERNAME']};dbname={$_ENV['DB_NAME']}";
+        $username = $_ENV['DB_USERNAME'];
+        $password = $_ENV['DB_PASSWORD'];
+
+        try {
+            self::$cnx = new \PDO($dsn, $username, $password);
+            self::$cnx->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+            echo 'Successsssssssssss!';
+            return self::$cnx;
+        } catch (\PDOException $e) {
+            die("Connection failed: " . $e->getMessage());
+        }
+    }
+}
+
+$db = new Database();
+$db->connect();
+?>
